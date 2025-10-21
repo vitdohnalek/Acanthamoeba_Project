fc5ba3bbd3ed4f5fa8f831511875527d7a5c57a6
Vít Dohnálek <vitdohnalek@gmail.com>
2025-10-21 11:33:35 +0200

    Add files via upload

---
+++ Utility/get_annotations.py
@@ -0,0 +1,19 @@
+from Bio import SeqIO
+
+
+annotations = {}
+for seq_rec in SeqIO.parse("proteins_annotated.fasta", "fasta"):
+	seq_ID = "_".join(seq_rec.id.split("|")[0].split("_")[:-1])
+	annotation = seq_rec.description.split("|")[1]
+	annotations[seq_ID] = annotation
+
+tsv = "IDs\tAnnotation\tbaseMean\tlog2FoldChange\tlfcSE\tpvalue\tpadj\n"
+with open("DE_results.txt", "r") as f:
+	for l in f:
+		if l.startswith("TRINITY_"):
+			tsv += l.split("\t")[0] + "\t"
+			tsv += annotations[l.split("\t")[0]] + "\t"
+			tsv += "\t".join(l.split("\t")[1:])
+
+with open("DE_results_annotated.tsv", "w") as f:
+	f.write(tsv)
---
+++ Utility/get_changed.py
@@ -0,0 +1,33 @@
+from Bio import SeqIO
+
+
+upregulated = []
+downregulated = []
+
+with open("upregulated.list", "r") as f:
+	for l in f:
+		if l.startswith("TRINITY_"):
+			upregulated.append(l.strip())
+
+with open("downregulated.list", "r") as f:
+	for l in f:
+		if l.startswith("TRINITY_"):
+			downregulated.append(l.strip())
+
+upregulated_fasta = ""
+downregulated_fasta = ""
+
+for seq_rec in SeqIO.parse("proteins_annotated.fasta", "fasta"):
+	seq_ID = "_".join(seq_rec.id.split("_")[:-1])
+	function = seq_rec.description.split("|")[1]
+	if  seq_ID in upregulated:
+		upregulated_fasta += f">{seq_ID}|{function}\n{seq_rec.seq.strip('*')}\n"
+		upregulated.remove(seq_ID)
+	elif seq_ID in downregulated:
+		downregulated_fasta += f">{seq_ID}|{function}\n{seq_rec.seq.strip('*')}\n"
+		downregulated.remove(seq_ID)
+
+with open("downregulated_proteins.fasta", "w") as f:
+	f.write(downregulated_fasta)
+with open("upregulated_proteins.fasta", "w") as f:
+	f.write(upregulated_fasta)
